37294902ff3c56e47bdf86ff9068d8ac4833e8d9
Lars Vogel <Lars.Vogel@vogella.com>
2017-04-11 14:10:02 +0200

    Adds exercises for a receiver test

---
+++ com.vogella.android.test.juntexamples/app/src/androidTest/java/com/vogella/android/test/juntexamples/mockito/OutgoingCallReceiverTest.java
@@ -0,0 +1,41 @@
+package com.vogella.android.test.juntexamples.mockito;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.vogella.android.test.juntexamples.OutgoingCallReceiver;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class OutgoingCallReceiverTest {
+    private OutgoingCallReceiver mReceiver;
+    private Context mContext;
+
+    @Before
+    public void setUp() {
+        mReceiver = new OutgoingCallReceiver();
+        // TODO create mContext mock;
+    }
+
+
+
+    @Test
+    public void activityShouldGetCorrectIntentData() {
+        // prepare data for onReceive and call it
+        Intent intent = new Intent(Intent.ACTION_NEW_OUTGOING_CALL);
+        intent.putExtra(Intent.EXTRA_PHONE_NUMBER, "01234567890");
+
+        mReceiver.onReceive(mContext, intent);
+        // assert that resultData from mReceiver
+
+        // what did receiver do?
+        // Use ArgumentCaptor.forClass to capture the intent
+        // Fill the argument with the correct verify call
+        // Extract the intent data for the test
+
+    }
+}
