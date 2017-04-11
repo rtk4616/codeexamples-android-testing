9e8b4d5143810998899ff31341c672d1374d9d46
Lars Vogel <Lars.Vogel@vogella.com>
2017-04-11 14:09:18 +0200

    Exercise Hamcrest with custom matcher

---
+++ com.vogella.android.test.juntexamples/app/src/test/java/com/vogella/android/test/juntexamples/hamcresttests/HamcrestTestsWithCustomMatcher.java
@@ -0,0 +1,19 @@
+package com.vogella.android.test.juntexamples.hamcresttests;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+
+
+public class HamcrestTestsWithCustomMatcher {
+
+
+    @Test
+    public void testRegularExpression( ) {
+        String s ="aaabbbaaaa";
+        // ensure that it matches "a*b*a*"
+        fail("not yet implemented");
+    }
+
+}
